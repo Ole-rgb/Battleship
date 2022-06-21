d8d3c0484936c4b0aaa699fb40a2e90499bef0d2
Ole <ole.nelson.r@gmail.com>
2022-06-21 14:03:02 +0200

    Prepared Implementation

--- Battleship/src/main/java/de/uni_hannover/hci/battleship/battleship/Main.java
+++
@@ -1,9 +0,0 @@
-package de.uni_hannover.hci.battleship.battleship;
-
-public class Main {
-    public static void main(String[] args){
-        String str = "Ole";
-        System.out.println("Hi "+ str);
-
-    }
-}
